23916f73a642cc167167def91fc7d08f616fbc5b
Ashley Sarah Eliassaint <ase330@nyu.edu>
2020-12-14 17:39:46 -0500

    Some new stuff
    Art, perhaps?

--- TheGameWithNoGraphics/Assets/Scripts/EndCollider.cs
+++ TheGameWithNoGraphics/Assets/Scripts/EndCollider.cs
@@ -1,28 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using MoonSharp.Interpreter.Debugging;
 using UnityEngine;
 
 public class EndCollider : MonoBehaviour
 {
-    public EndScene theEndingScript;
+    public bool startEnding = false;
+    public GameObject player;
+    
     // Start is called before the first frame update
     void Start()
     {
-        //theEndingScript = GetComponent<EndScene>();
+       player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startEnding == true)
+        {
+            Debug.Log("begin ending");
+            player.GetComponent<RectTransform>().localPosition = Vector3.up; 
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            theEndingScript.theEnd = true;
+            startEnding = true;
         }
-        Debug.Log("Collide");
+       
     }
 }
--- TheGameWithNoGraphics/Assets/Scripts/textFacePlayer.cs
+++ TheGameWithNoGraphics/Assets/Scripts/textFacePlayer.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class textFacePlayer : MonoBehaviour
 {
+    public GameObject player;
     public Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        target = player.transform;
     }
 
     // Update is called once per frame
